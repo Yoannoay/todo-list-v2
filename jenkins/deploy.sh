a0fe76851435de1577d58b2d038cb15f30cf386a
Ubuntu <yoanc@devxjenkins.ud0p2mrj02fengrxjfn4vqmcsb.parx.internal.cloudapp.net>
2021-11-26 10:46:04 +0000

    Sending env vars through ssh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -4,4 +4,7 @@ echo "Deploy stage... now!"
 
 
 scp docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
-ssh jenkins@swarm-manager docker stack deploy --compose-file docker-compose.yaml todo-app
+
+ssh jenkins@swarm-manager \
+    DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
+    docker stack deploy --compose-file docker-compose.yaml todo-app
