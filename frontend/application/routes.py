5bb5c919a77c6089fe80f3147046cf7b37cccd0a
Ubuntu <yoanc@devxjenkins.ud0p2mrj02fengrxjfn4vqmcsb.parx.internal.cloudapp.net>
2021-11-26 10:52:35 +0000

    Trying to get frontend to work (linking it to backend host)

--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -3,11 +3,11 @@ from application import app
 from application.forms import TaskForm
 from flask import render_template, request, redirect, url_for, jsonify
 
-
+backend_host = "todo-app_backend:5000"
 @app.route('/')
 @app.route('/home')
 def home():
-    all_tasks = requests.get("http://todo-app-backend:5000/read/allTasks").json()
+    all_tasks = requests.get("http://{backend_host}/read/allTasks").json()
     return render_template('index.html', title="Home", all_tasks=all_tasks)
 
 # @app.route('/create/task', methods=['GET','POST'])
