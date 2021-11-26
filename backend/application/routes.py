186f6fe687acec0118ef55b161927b89a962d6b2
Ubuntu <yoanc@devxjenkins.ud0p2mrj02fengrxjfn4vqmcsb.parx.internal.cloudapp.net>
2021-11-26 13:39:16 +0000

    Setting ENV variables

--- backend/application/routes.py
+++ backend/application/routes.py
@@ -21,7 +21,7 @@ def read_tasks():
     for task in all_tasks:
         tasks_dict["tasks"].append(
             {
-                "id": "task.id"
+                "id": task.id,
                 "description": task.description,
                 "completed": task.completed
             }
--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -7,4 +7,6 @@ scp docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 
 ssh jenkins@swarm-manager \
     DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
+    DATABASE_URI=$DATABASE_URI \
+    CREATE_SCHEMA=true \
     docker stack deploy --compose-file docker-compose.yaml todo-app
